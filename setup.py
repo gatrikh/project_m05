429ae2d16ce2951334ae239773e4e71b3a0e2e8a
Gary <gary@MacBook-Pro-de-Gary.local>
2021-09-06 16:10:35 +0200

    Powerpoint presentation draft

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     packages=find_packages(),
     version='0.1.0',
     description='Project M05',
-    author='Project M05',
-    license='',
+    author='Lucas Devanthery and Gary Folli',
+    license='None',
 )
