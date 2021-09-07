b1bf212660f813c2795df5b35d22f8b7c8ffe746
Gary <gary@MacBook-Pro-de-Gary.local>
2021-09-08 01:05:59 +0200

    Improvements of normalize, pca and test_split test

--- tests/test.py
+++ tests/test.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 from pandas.core.frame import DataFrame
 from sklearn.model_selection import train_test_split
+import math
 
 from src.data import data as data
 from src.preprocessing import preprocessing as preprocess
@@ -30,24 +31,70 @@ class TestPreprocessing(unittest.TestCase):
 
         for col in self.df.columns: 
             self.assertEqual(self.df[col].dtype == "object", False, "Should be False")
+    
+    def test_normalize(self): 
+
+        df_norm = preprocess.normalize(self.df)
+
+        array_1 = df_norm["dst_bytes"].to_numpy()
+        array_2 = (df_norm["dst_bytes"].to_numpy() - np.mean(df_norm["dst_bytes"].to_numpy())/np.std(df_norm["dst_bytes"].to_numpy()))
+
+        np.testing.assert_allclose(array_1, array_2, err_msg="Normalization test failed", atol=0.0005)
+
+    def test_pca_reduction(self): 
+        
+        df_norm = preprocess.normalize(self.df)
+        df_pca = preprocess.pca_reduction(df_norm)
+        self.assertEqual(df_pca.shape[1], 21, "Should be 21")
+        self.assertEqual("label" in df_pca.columns, True, "Should be True")
 
     def test_df_split(self):
+
+        nbr_label = pd.unique(self.df["label"])
+        nbr_label = nbr_label.sort_values()
+
+        list_len_train = []
+        list_len_val = []
+        list_len_test = []
+
+        for label in nbr_label:
+
+            len_subset = self.df[self.df["label"] == label].shape[0]
         
-        train_labelCounts = self.train_df["label"].cat.remove_unused_categories().value_counts().to_numpy()
-        val_labelCounts = self.val_df["label"].cat.remove_unused_categories().value_counts().to_numpy()
-        test_labelCounts = self.test_df["label"].cat.remove_unused_categories().value_counts().to_numpy()
-        
-        train_ratio = train_labelCounts/(train_labelCounts+val_labelCounts+test_labelCounts)
-        val_ratio = val_labelCounts/(train_labelCounts+val_labelCounts+test_labelCounts)
-        test_ratio = test_labelCounts/(train_labelCounts+val_labelCounts+test_labelCounts)
-        
-        train_ratio_ref = np.full(np.shape(train_ratio), 0.6)
-        val_ratio_ref = np.full(np.shape(val_ratio), 0.2)
-        test_ratio_ref = np.full(np.shape(test_ratio), 0.2)
-        
-        np.testing.assert_allclose(train_ratio, train_ratio_ref,err_msg = "split test failed on train_set", atol = 0.05)
-        np.testing.assert_allclose(val_ratio, val_ratio_ref, err_msg = "split test failed on val_set",atol = 0.05)
-        np.testing.assert_allclose(test_ratio, test_ratio_ref, err_msg = "split test failed on test_set",atol = 0.05)
+            if len_subset >= 4:
+                
+                len_train = math.floor(0.6 * len_subset)
+                len_val = math.floor((len_subset - len_train)*0.5)
+                len_test = len_subset - len_train - len_val
+
+                list_len_train.append(len_train)
+                list_len_val.append(len_val)
+                list_len_test.append(len_test)
+
+            elif len_subset == 3: 
+
+                list_len_train.append(1)
+                list_len_val.append(1)
+                list_len_test.append(1)
+
+            elif len_subset == 2:
+                list_len_train.append(1)
+                list_len_val.append(1)
+                
+            elif len_subset == 1: 
+                list_len_train.append(1)
+
+        arr_len_train = np.array(list_len_train)
+        arr_len_val = np.array(list_len_val)
+        arr_len_test = np.array(list_len_test)
+
+        len_train_df = self.train_df["label"].value_counts().sort_index().to_numpy()
+        len_val_df = self.val_df["label"].value_counts().sort_index().to_numpy()
+        len_test_df = self.test_df["label"].value_counts().sort_index().to_numpy()
+
+        np.testing.assert_array_equal(arr_len_train, len_train_df, "Train arrays length not equal")
+        np.testing.assert_array_equal(arr_len_val, len_val_df, "Validation arrays length not equal")
+        np.testing.assert_array_equal(arr_len_test, len_test_df, "Test arrays length not equal")
         
 class TestModels(unittest.TestCase):
 
@@ -89,4 +136,10 @@ class TestModels(unittest.TestCase):
         self.assertEqual(acc > 0.999, True, "Should be True")
 
 if __name__ == '__main__':
+
+    # suite = unittest.TestSuite()
+    # suite.addTest(TestPreprocessing("test_df_split"))
+    # runner = unittest.TextTestRunner()
+    # runner.run(suite)
+
     unittest.main()
