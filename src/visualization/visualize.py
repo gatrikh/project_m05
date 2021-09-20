9c78c2881db84239f7ecbdf5b5231a591f5ecd1d
lucas devanthéry <devantheryl@windowslive.com>
2021-09-20 16:39:57 +0200

    confusion matrix function done

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import numpy as np
 from IPython.display import display
+from sklearn.metrics import plot_confusion_matrix
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 def dsp(data, rows=10, columns=None):
     
@@ -29,3 +32,30 @@ def accuracy(labels, pred, show=False):
         print(f"Incorrect: {incorrect}")
 
     return acc, correct, incorrect
+
+def get_confusion_matrix(classifier,x_test,y_test,save_fig = False,savefile_name = ""):
+    
+    fig2, ax2 = plt.subplots(figsize=(15, 15))
+    disp2 = plot_confusion_matrix(classifier, x_test, y_test, ax=ax2,normalize = "true")
+    disp2.ax_.set_title("Confusion matrix, without normalization")
+    
+    fig, ax = plt.subplots(figsize=(15, 15))
+    disp1 = plot_confusion_matrix(classifier, x_test, y_test, ax=ax,normalize = None)
+    disp1.ax_.set_title("Confusion matrix, without normalization")
+    disp1.ax_.autoscale
+    
+    
+
+    if save_fig :
+        if savefile_name != "":
+            savefile_name_normalized = "normalized_" + savefile_name
+            savefile_name_unnormalized = "unnormalized_" + savefile_name
+            fig2.savefig(savefile_name_normalized)
+            fig.savefig(savefile_name_unnormalized)
+            print("figure normalized saved under : ",savefile_name_normalized)
+            print("figure unnormalized saved under : ",savefile_name_unnormalized)
+        else:
+            print("please give a name for your file to save")
+            
+    plt.show()
+    
